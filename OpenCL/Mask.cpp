c23b90aa85aaa9728bae34ccfcd78ce3fd8faefa
steve628 <s0558040@htw-berlin.de>
2019-12-20 17:13:06 +0100

    Got gaussion blur with cpu working

--- OpenCL/Mask.cpp
+++ OpenCL/Mask.cpp
@@ -2,16 +2,12 @@
 #include "Mask.h"
 
 float* Mask::createBlurMask(const float sigma, int* maskSizePointer) {
-	int maskSize = (int)ceil(2.57f * sigma);
-	double s = (2 * sigma * sigma);
+	int maskSize = (int)ceil(3.0f * sigma);
 	float* mask = new float[(maskSize * 2 + 1) * (maskSize * 2 + 1)];
-	float dsq;
-	float temp;
 	float sum = 0.0f;
 	for (int a = -maskSize; a < maskSize + 1; a++) {
 		for (int b = -maskSize; b < maskSize + 1; b++) {
-			dsq = (a * a + b * b);
-			temp = exp(-dsq / s) / (M_PI * s);
+			float temp = exp(-((float)(a * a + b * b) / (2 * sigma * sigma)));
 			sum += temp;
 			mask[a + maskSize + (b + maskSize) * (maskSize * 2 + 1)] = temp;
 		}
--- OpenCL/OpenCL.cpp
+++ OpenCL/OpenCL.cpp
@@ -45,25 +45,58 @@ int main(int argc, char** argv)
 	int maskSize;
 	float* mask = Mask::createBlurMask(2.0f, &maskSize);
 
+	/*
 	printf("Maske\n");
 	for (int a = -maskSize; a < maskSize + 1; a++) {
 		for (int b = -maskSize; b < maskSize + 1; b++) {
 			printf("%f, ",mask[a + maskSize + (b + maskSize) * (maskSize * 2 + 1)]);
 		}
 	}
-	printf("\n");
-
+	printf("\n"); 
+	*/
+	/*
 	t.start("GaussianBlur");
-	OpenCLProgram prog("TestKernel.cl", "coolTest");
+	OpenCLProgram prog("GaussianBlur.cl", "gaussianBlur");
 	prog.setImage2D(0, (size_t)(originalImg.cols), (size_t)(originalImg.rows), (size_t)(originalImg.channels()), originalImg.data, true);
 	prog.setImage2D(1, (size_t)(outImg.cols), (size_t)(outImg.rows), (size_t)(outImg.channels()), outImg.data, false);
-	//prog.setArg(2, mask, sizeof(float) * (maskSize * 2 + 1) * (maskSize * 2 + 1), true);
-	//prog.setArg(3, maskSize);
+	prog.setArg(2, mask, sizeof(float) * (maskSize * 2 + 1) * (maskSize * 2 + 1), true);
+	prog.setArg(3, maskSize);
 	prog.startProgram(cl::NDRange(originalImg.cols, originalImg.rows));
 	prog.getResults();
 	t.stop();
+	*/
 
-	cv::GaussianBlur(originalImg, openCVGaus, cv::Size(5, 5), 2);
+	printf("Bild, blue channel\n");
+	int channels = originalImg.channels();
+	float sum_b, sum_g, sum_r;
+	for (int y = 0; y < originalImg.rows; y++) {
+		for (int x = 0; x < originalImg.cols; x++) {
+			sum_b = 0.0f;
+			sum_g = 0.0f;
+			sum_r = 0.0f;
+			for (int a = -maskSize; a < maskSize + 1; a++) {
+				for (int b = -maskSize; b < maskSize + 1; b++) {
+					if (y + a < 0 || y + a >= originalImg.rows) {
+						continue;
+					}
+					else if (x + b < 0 || x + b >= originalImg.cols) {
+						continue;
+					}
+					else {
+						sum_b += mask[a + maskSize + (b + maskSize) * (maskSize * 2 + 1)] * originalImg.data[(y + a) * originalImg.cols * channels + (x + b) * channels + 0];
+						sum_g += mask[a + maskSize + (b + maskSize) * (maskSize * 2 + 1)] * originalImg.data[(y + a) * originalImg.cols * channels + (x + b) * channels + 1];
+						sum_r += mask[a + maskSize + (b + maskSize) * (maskSize * 2 + 1)] * originalImg.data[(y + a) * originalImg.cols * channels + (x + b) * channels + 2];
+					}
+				}
+			}
+			outImg.data[y * originalImg.cols * channels + x * channels + 0] = (uint8_t)(sum_b);
+			outImg.data[y * originalImg.cols * channels + x * channels + 1] = (uint8_t)(sum_g);
+			outImg.data[y * originalImg.cols * channels + x * channels + 2] = (uint8_t)(sum_r);
+		}
+	}
+	
+
+	cv::GaussianBlur(originalImg, openCVGaus, cv::Size(5, 5), 2, 2);
 
 	cv::Mat delta(originalImg.rows, originalImg.cols, CV_8UC3);
 	delta = originalImg - openCVGaus;
@@ -72,6 +105,7 @@ int main(int argc, char** argv)
 	cv::imwrite(currentFolder + "\\Out_Blur.png", outImg);
 	cv::imwrite(currentFolder + "\\delta.png", delta);
 	cv::imwrite(currentFolder + "\\Gaus.png", openCVGaus);
+	delete mask;
 
 	/*
 	t.start("BGR zu YCrCb");
